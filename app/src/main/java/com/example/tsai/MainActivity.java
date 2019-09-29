70ef3d0bb404fe5305e7526a7ab40148fcec6a7b
leviliang <levi947tw@gmail.com>
2019-09-29 23:34:50 +0800

    .gitignore is now working

--- app/src/main/java/com/example/tsai/MainActivity.java
+++ app/src/main/java/com/example/tsai/MainActivity.java
@@ -1,13 +1,15 @@
 package com.example.tsai;
 
 import android.os.Bundle;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
